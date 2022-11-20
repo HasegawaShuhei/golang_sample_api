ea7511c4baf5bd4e752122148675eaca57de4cba
HasegawaShuhei <sh1027.dev@gmail.com>
2022-11-20 16:14:33 +0900

    env周りの調整

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/HasegawaShuhei/golang_sample_api/database"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("env load failed %v", err)
+	}
 	db := database.OpenDB(os.Getenv("DRIVER"), os.Getenv("DSN"))
 	defer database.CloseDB(db)
 
