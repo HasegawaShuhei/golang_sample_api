a463fe2b3b8c6f592c953d6be7f9dc24efa3c174
HasegawaShuhei <sh1027.dev@gmail.com>
2022-11-21 22:41:34 +0900

    auth_controller追加と動作確認

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,27 +1,26 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
-
+	"github.com/HasegawaShuhei/golang_sample_api/controller"
 	"github.com/HasegawaShuhei/golang_sample_api/database"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/joho/godotenv"
+	"gorm.io/gorm"
+)
+
+var (
+	db             *gorm.DB                  = database.OpenDB()
+	authController controller.AuthController = controller.NewAuthController()
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("env load failed %v", err)
-	}
-	db := database.OpenDB(os.Getenv("DRIVER"), os.Getenv("DSN"))
 	defer database.CloseDB(db)
 
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "OK")
-	})
+	authRoutes := r.Group("v1/auth")
+	{
+		authRoutes.POST("/login", authController.Login)
+		authRoutes.POST("/register", authController.Register)
+	}
 	r.Run()
 }
--- database/connection.go
+++ database/connection.go
@@ -3,12 +3,19 @@ package database
 import (
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-func OpenDB(driver string, dsn string) *gorm.DB {
+func OpenDB() *gorm.DB {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("env load failed %v", err)
+	}
+	dsn := os.Getenv("DSN")
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("OpenDB failed:", err)
