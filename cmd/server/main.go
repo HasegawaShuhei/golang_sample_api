6f02f66220364f1c1e94fd493f59abbbd59a0f47
HasegawaShuhei <sh1027.dev@gmail.com>
2022-11-24 23:21:44 +0900

    user周りの処理追加

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/HasegawaShuhei/golang_sample_api/controller"
 	"github.com/HasegawaShuhei/golang_sample_api/database"
+	"github.com/HasegawaShuhei/golang_sample_api/middleware"
 	"github.com/HasegawaShuhei/golang_sample_api/repository"
 	"github.com/HasegawaShuhei/golang_sample_api/service"
 	"github.com/gin-gonic/gin"
@@ -15,7 +16,9 @@ var (
 	userRepository repository.UserRepository = repository.NewUserRepository(db)
 	authService    service.AuthService       = service.NewAuthService(userRepository)
 	jwtService     service.JWTService        = service.NewJWTService()
+	userService    service.UserService       = service.NewUserService(userRepository)
 	authController controller.AuthController = controller.NewAuthController(authService, jwtService)
+	userController controller.UserController = controller.NewUserController(userService, jwtService)
 )
 
 func main() {
@@ -26,6 +29,12 @@ func main() {
 	{
 		authRoutes.POST("/login", authController.Login)
 		authRoutes.POST("/register", authController.Register)
+	}
+
+	userRoutes := r.Group("v1/user", middleware.AuthorizeJWT(jwtService))
+	{
+		userRoutes.GET("/profile", userController.Profile)
+		userRoutes.PUT("/profile", userController.Update)
 	}
 	r.Run()
 }
