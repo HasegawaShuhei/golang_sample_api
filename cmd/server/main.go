9fc7f0e7f2a8eeb01185a44d9f1a99847649543d
HasegawaShuhei <sh1027.dev@gmail.com>
2022-11-25 00:31:25 +0900

    book周りの処理追加

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -14,17 +14,21 @@ import (
 var (
 	db             *gorm.DB                  = database.OpenDB()
 	userRepository repository.UserRepository = repository.NewUserRepository(db)
-	authService    service.AuthService       = service.NewAuthService(userRepository)
+	bookRepository repository.BookRepository = repository.NewBookRepository(db)
 	jwtService     service.JWTService        = service.NewJWTService()
 	userService    service.UserService       = service.NewUserService(userRepository)
+	bookService    service.BookService       = service.NewBookService(bookRepository)
+	authService    service.AuthService       = service.NewAuthService(userRepository)
 	authController controller.AuthController = controller.NewAuthController(authService, jwtService)
 	userController controller.UserController = controller.NewUserController(userService, jwtService)
+	bookController controller.BookController = controller.NewBookController(bookService, jwtService)
 )
 
 func main() {
 	defer database.CloseDB(db)
 
 	r := gin.Default()
+
 	authRoutes := r.Group("v1/auth")
 	{
 		authRoutes.POST("/login", authController.Login)
@@ -36,5 +40,15 @@ func main() {
 		userRoutes.GET("/profile", userController.Profile)
 		userRoutes.PUT("/profile", userController.Update)
 	}
+
+	bookRoutes := r.Group("v1/books", middleware.AuthorizeJWT(jwtService))
+	{
+		bookRoutes.GET("/", bookController.All)
+		bookRoutes.POST("/", bookController.Insert)
+		bookRoutes.GET("/:id", bookController.FindByID)
+		bookRoutes.PUT("/:id", bookController.Update)
+		bookRoutes.DELETE("/:id", bookController.Delete)
+	}
+
 	r.Run()
 }
